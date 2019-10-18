b18444eac9d7769c970bb796345f17fe687008e7
fpareja-lecaros <fpareja-lecaros@art.com>
2019-10-17 18:00:20 -0700

    Added more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,11 +33,11 @@ values(11, 'Fun Flair', 'UI_011.png', 'DECOR','');
 
 
 insert into room
-values('defaultRoom', '01', 'Table,Frame,ChairLeft,ChairRight');
+values('defaultRoom', '01', 'LeftWall,Frame,ChairLeft,ChairRight,BackWall,Rug,RightCorner,LeftCorner,Vase,Pillow');
 
 
 insert into region
-values('Table', '264, 734, 397, 734, 231, 859, 99, 859', 'TABLE', 'poly');
+values('LeftWall', '264, 734, 397, 734, 231, 859, 99, 859', 'TABLE', 'poly');
 
 insert into region
 values('ChairLeft', '342,335,720,800', 'CHAIR', 'rect');
@@ -48,6 +48,24 @@ values('ChairRight', '880,330,1280,820', 'CHAIR', 'rect');
 insert into region
 values('Frame', '925, 378, 661, 0', 'ART', 'rect');
 
+insert into region
+values('Rug', '565,768,1163,869', 'RUG', 'rect');
+
+insert into region
+values('BackWall', '638,678,586,734,1007,741,980,681', 'BENCH,TABLE', 'poly');
+
+insert into region
+values('RightCorner', '1168,677,1126,721,1251,721,1223,676,1222,165,1305,162,1280,10,1103,13,1076,166,1164,165', 'LAMP', 'poly');
+
+insert into region
+values('LeftCorner', '358,214,544,692', 'DECOR', 'rect');
+
+insert into region
+values('Pillow', '63,627,277,533,483,545,313,639', 'DECOR', 'poly');
+
+insert into region
+values('Vase', '731,488,1138,568,1259,363', 'DECOR', 'poly');
+
 
 insert into room_configuration
 values('', '01');
@@ -57,3 +75,33 @@ values('ChairLeft:3', '03');
 
 insert into room_configuration
 values('ChairRight:3', '02');
+
+insert into room_configuration
+values('BackWall:2,ChairRight:3', '05');
+
+insert into room_configuration
+values('ChairRight:3,LeftWall:2', '06');
+
+insert into room_configuration
+values('ChairRight:3,LeftWall:1', '07');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftWall:1', '08');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftCorner:4,LeftWall:1', '09');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftCorner:4,LeftWall:1,Rug:7', '10');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftCorner:4,LeftWall:1,RightCorner:8,Rug:7', '11');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftCorner:4,LeftWall:1,RightCorner:8,Rug:7,Vase:11', '13');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,LeftCorner:4,LeftWall:1,Pillow:6,RightCorner:8,Rug:7,Vase:11', '14');
+
+insert into room_configuration
+values('BackWall:10,ChairRight:3,Frame:9,LeftCorner:4,LeftWall:1,Pillow:6,RightCorner:8,Rug:7,Vase:11', '15a')
